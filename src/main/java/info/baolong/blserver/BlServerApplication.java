ecb86f1a9aba676ddfd0a891bb72f226aea00d50
1192384722@qq.com <54321miao>
2022-08-01 18:34:27 +0800

    初始化提交

---
+++ src/main/java/info/baolong/blserver/BlServerApplication.java
@@ -0,0 +1,13 @@
+package info.baolong.blserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/baolong/blserver/BlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package info.baolong.blserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
